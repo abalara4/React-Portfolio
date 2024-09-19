69ff1ce074e06df1ed3c2870115d4c294a6bf39f
abalara4 <abalara4@gmail.com>
2024-09-19 10:12:10 -0400

    Updated files

---
+++ React Portfolio/src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <section>
+            <h1>About Me</h1>
+            <p>
+                I am an aspiring web developer.  I am currently enrolled in the Rutger's University Coding Bootcamp.  I am learning HTML, CSS, and JavaScript.  I am also learning how to use React, Node.js, and Express.js and more.  I am excited to learn more about web development and to start a new career as a web developer.
+            </p>
+        </section>
+    );
+}
+
+export default About;
